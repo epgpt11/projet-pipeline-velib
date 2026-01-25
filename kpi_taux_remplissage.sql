dd0a409ed0fea66127c6e371bc392766c9a183d7
Seulgi SHIN <48745528+epgpt11@users.noreply.github.com>
2026-01-25 11:26:42 +0100

    Create kpi_taux_remplissage.sql

---
+++ kpi_taux_remplissage.sql
@@ -0,0 +1,14 @@
+SELECT
+  ingested_ts,
+  station_id,
+  name,
+  arrondissement,
+  bikes_i AS bikes,
+  docks_i AS docks,
+  (bikes_i + docks_i) AS capacity_calc,
+  fill_rate
+FROM velib_station_typed
+WHERE is_installed_i = 1
+  AND ingested_ts IS NOT NULL
+  AND bikes_i IS NOT NULL
+  AND docks_i IS NOT NULL;

9e1b8c1142f1f44baa9c07ccd9abcd5fded3eedc
Seulgi SHIN <48745528+epgpt11@users.noreply.github.com>
2026-01-25 11:23:21 +0100

    Update create_velib_station_typed.sql
    correction type error

--- create_velib_station_typed.sql
+++ create_velib_station_typed.sql
@@ -4,8 +4,27 @@ SELECT
   station_id,
   name,
   arrondissement,
-  try_cast(is_installed AS integer) AS is_installed_i,
+
+  CASE
+    -- if string
+    WHEN upper(trim(is_installed)) = 'OUI' THEN 1
+    WHEN upper(trim(is_installed)) = 'NON' THEN 0
+
+    -- if boolean
+    WHEN lower(trim(is_installed)) IN ('true', 't', 'yes', 'y') THEN 1
+    WHEN lower(trim(is_installed)) IN ('false', 'f', 'no', 'n') THEN 0
+
+    -- if number
+    WHEN trim(is_installed) = '1' THEN 1
+    WHEN trim(is_installed) = '0' THEN 0
+
+    ELSE NULL
+  END AS is_installed_i,
+
   try_cast(bikes AS integer) AS bikes_i,
   try_cast(docks AS integer) AS docks_i,
-  try_cast(bikes AS double) / NULLIF(try_cast(bikes AS double) + try_cast(docks AS double), 0) AS fill_rate
+
+  try_cast(bikes AS double)
+    / NULLIF(try_cast(bikes AS double) + try_cast(docks AS double), 0) AS fill_rate
+
 FROM velib_station_flat;
